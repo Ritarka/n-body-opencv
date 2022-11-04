56ac28078a1abc035aaf5d7c8cf8e93ef8894c79
Ritarka <ritarkasamanta@gmail.com>
2022-11-04 09:47:30 -0400

    some features

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 
 #include <omp.h>
 
-//#include <cv.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/features2d.hpp>
@@ -23,48 +22,44 @@
 using namespace std;
 using namespace cv;
 
-typedef struct{
+typedef struct {
     double x, y, mass;
-    double vx,vy;} Body;
+    double vx,vy;
+    Scalar color;
+} Body;
 
-void randomlyAllocate(Body *data, int n)
-{
-for (int i=0; i<n; i++){
-    data[i].x = ( std::rand() % ( 100 ));
-    data[i].y = ( std::rand() % ( 100 ));
-    data[i].mass = ( std::rand() % (3)) + 1;
-    //data[i].mass = 1;
+void randomlyAllocate(Body *data, int n) {
+    for (int i = 0; i < n; i++) {
+        data[i].x = (rand() %  100);
+        data[i].y = (rand() %  100);
+        data[i].mass = (rand() % 3) + 1;
+        data[i].color = Scalar(rand() % 255, rand() % 255, rand() % 255);
     }
 }
 
-void calcForce(Body *p, float dt, int numBodies)
-{
+void calcForce(Body *p, float dt, int numBodies) {
 #pragma omp parallel for schedule(dynamic)
-for (int i=0; i<numBodies; i++){
-    double fx = 0.0f;
-    double fy = 0.0f;
+    for (int i = 0; i < numBodies; i++){
+        double fx = 0.0f;
+        double fy = 0.0f;
 
-    for(int j=0; j<numBodies; j++){
-        double dx = p[j].x - p[i].x;
-        double dy = p[j].y - p[i].y;
-        double sqdist = dx*dx + dy*dy + SOFTENING;
-        double InvDist = 1.0f / (sqrtf(sqdist));
-        double InvDist3 = InvDist* InvDist* InvDist;
-        fx += (dx * InvDist3 * p[j].mass) / p[i].mass ;
-        fy += (dy * InvDist3 * p[j].mass) / p[i].mass ;
-    }
+        for (int j = 0; j < numBodies; j++){
+            double dx = p[j].x - p[i].x;
+            double dy = p[j].y - p[i].y;
+            double sqdist = dx*dx + dy*dy + SOFTENING;
+            double InvDist = 1.0f / sqrtf(sqdist);
+            double InvDist3 = InvDist * InvDist * InvDist;
+            fx += (dx * InvDist3 * p[j].mass) / p[i].mass ;
+            fy += (dy * InvDist3 * p[j].mass) / p[i].mass ;
+        }
 
-    p[i].vx += dt*fx;
-    p[i].vy += dt*fy;
-
-    if(p[i].x < 0 || p[i].x > 100 ){ p[i].vx *= -1;}
-    if(p[i].y < 0 || p[i].y > 100 ){ p[i].vy *= -1;}
+        p[i].vx += dt*fx;
+        p[i].vy += dt*fy;
 
     }
 }
 
-int main()
-    {
+int main() {
     double scale_x = WORLD_MAX_X / 100;
     double scale_y = WORLD_MAX_Y / 100;
 
@@ -72,7 +67,6 @@ int main()
     disp = Scalar(128, 128, 128);
     int nBodies = NUM_PARTICLES;
     double dt = 0.1;
-    int nIter = 100000;
 
     int bytes = nBodies*sizeof(Body);
     double *buf = (double*)malloc(bytes);
@@ -81,22 +75,58 @@ int main()
     randomlyAllocate(p, nBodies);
 
     imshow("N-Particles Gravity Simulation", disp);
-    for(int iter=0; iter<=nIter; iter++){
+
+    int step = 0;
+    int prev = 0;
+
+    for (int iter = 0; ; iter++) {
         disp = Scalar(0, 0, 0);
+        dt = abs(dt);
+        if (prev) {
+            dt *= -1;
+            iter -= 2;
+            if (iter < 0) {
+                iter = 0;
+                goto input;
+            }
+        }
         calcForce(p, dt, nBodies);
 
         #pragma omp parallel for schedule(dynamic)
-        for(int i=0; i<nBodies; i++){
-                p[i].x += (int)p[i].vx*dt;
-                p[i].y += (int)p[i].vy*dt;
-                circle( disp, Point(round(p[i].x * scale_x), round(p[i].y * scale_y)), p[i].mass, Scalar(255,255,255), -1);
+        for (int i = 0; i < nBodies; i++) {
+            p[i].x += (int)p[i].vx * dt;
+            p[i].y += (int)p[i].vy * dt;
+            circle( disp, Point(round(p[i].x * scale_x), round(p[i].y * scale_y)), p[i].mass, p[i].color, -1);
         }
 
         putText(disp, "Epoch: " + to_string(iter), Point(10, 30), FONT_HERSHEY_COMPLEX, 0.5, Scalar(255, 255, 255), 1, LINE_8, false);
         imshow("N-Particles Gravity Simulation", disp);
 
-        if( waitKey(30) == 'q')
-            break;
+        input:
+        char c = (char)waitKey(30);
+        if (c == 'q') {
+            return 0;
+        } else if (step || c == 'p') {
+            step = 0;
+            prev = 0;
+            while (1) {
+                c = (char)waitKey(0);
+                if (c == 'p')
+                    break;
+                else if (c == 'q')
+                    return 0;
+                else if (c == 's') {
+                    //imwrite("screenshot.png", disp);
+                } else if (c == 'n') {
+                    step = 1;
+                    break;
+                } else if (c == 'b') {
+                    prev = 1;
+                    step = 1;
+                    break;
+                }
+            }
+        }
 
     }
 }
