74fd6cfaaeb266bbd0349cbb441a1091c97f70fd
Ritarka <ritarkasamanta@gmail.com>
2022-11-11 10:50:42 -0500

    better randomization feature

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string>
 #include <ncurses.h>
+#include <random>
 
 #include <omp.h>
 
@@ -17,7 +18,7 @@
 #define WORLD_MAX_Y 800
 
 #define SOFTENING 2
-#define NUM_PARTICLES 2000
+#define NUM_PARTICLES 1000
 
 #define DT 0.1
 
@@ -32,10 +33,15 @@ typedef struct {
 } Body;
 
 void randomlyAllocate(Body *data, int n) {
+    double lower_bound = 0;
+    std::uniform_real_distribution<double> unifx(lower_bound, WORLD_MAX_X);
+    std::uniform_real_distribution<double> unify(lower_bound, WORLD_MAX_X);
+    std::default_random_engine re;
+    
     for (int i = 0; i < n; i++) {
-        data[i].x = rand() % 100;
-        data[i].y = rand() % 100;
-        data[i].mass = rand() % 3 + 1;
+        data[i].x = unifx(re);
+        data[i].y = unify(re);
+        data[i].mass = rand() % 4 + 1;
         data[i].color = Scalar(rand() % 255, rand() % 255, rand() % 255);
     }
 }
@@ -47,8 +53,8 @@ void calcForce(Body *p, float dt, int numBodies) {
         double fy = 0.0f;
 
         for (int j = 0; j < numBodies; j++) {
-            double dx = p[j].x - p[i].x;
-            double dy = p[j].y - p[i].y;
+            double dx = (p[j].x - p[i].x) / 100;
+            double dy = (p[j].y - p[i].y) / 100;
             double sqdist = dx*dx + dy*dy + SOFTENING;
             double InvDist = 1.0f / sqrtf(sqdist);
             double InvDist3 = InvDist * InvDist * InvDist;
@@ -63,42 +69,32 @@ void calcForce(Body *p, float dt, int numBodies) {
 }
 
 int main() {
-    double scale_x = WORLD_MAX_X / 100;
-    double scale_y = WORLD_MAX_Y / 100;
+    double scale_x = 100;
+    double scale_y = 100;
 
     Mat disp(Mat(WORLD_MAX_Y, WORLD_MAX_X, CV_8UC3));
     disp = Scalar(128, 128, 128);
     int nBodies = NUM_PARTICLES;
     double dt = 0.1;
 
-    double *buf = (double*)malloc(nBodies * sizeof(Body));
-    Body *p = (Body*)buf;
+    Body *p = (Body*)malloc(nBodies * sizeof(Body));
 
     randomlyAllocate(p, nBodies);
 
     imshow("N-Particles Gravity Simulation", disp);
 
     int step = 0;
-    int prev = 0;
 
     for (int iter = 0; ; iter++) {
         disp = Scalar(0, 0, 0);
         dt = abs(dt);
-        if (prev) {
-            dt *= -1;
-            iter -= 2;
-            if (iter < 0) {
-                iter = 0;
-                goto input;
-            }
-        }
         calcForce(p, dt, nBodies);
 
         #pragma omp parallel for schedule(dynamic)
         for (int i = 0; i < nBodies; i++) {
             p[i].x += (int)p[i].vx * dt;
             p[i].y += (int)p[i].vy * dt;
-            circle( disp, Point(round(p[i].x * scale_x), round(p[i].y * scale_y)), p[i].mass, p[i].color, -1);
+            circle( disp, Point(round(p[i].x), round(p[i].y)), p[i].mass, p[i].color, -1);
         }
 
         putText(disp, "Epoch: " + to_string(iter), Point(10, 30), FONT_HERSHEY_COMPLEX, 0.5, Scalar(255, 255, 255), 1, LINE_8, false);
@@ -110,7 +106,7 @@ int main() {
             return 0;
         } else if (step || c == 'p') {
             step = 0;
-            prev = 0;
+
             while (1) {
                 c = (char)waitKey(0);
                 if (c == 'p')
@@ -123,13 +119,8 @@ int main() {
                 } else if (c == 'n') {
                     step = 1;
                     break;
-                } /*else if (c == 'b') {
-                    prev = 1;
-                    step = 1;
-                    break;
-                }*/
+                }
             }
         }
-
     }
 }
