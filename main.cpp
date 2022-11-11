31380e198bb14b83ab15317e238a19bd1b85b9d0
daray21 <77138061+daray21@users.noreply.github.com>
2022-11-11 11:03:40 -0500

    testyts
    testy

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string>
 #include <ncurses.h>
-#include <random>
+#include <fstream>
 
 #include <omp.h>
 
@@ -33,15 +33,10 @@ typedef struct {
 } Body;
 
 void randomlyAllocate(Body *data, int n) {
-    double lower_bound = 0;
-    std::uniform_real_distribution<double> unifx(lower_bound, WORLD_MAX_X);
-    std::uniform_real_distribution<double> unify(lower_bound, WORLD_MAX_X);
-    std::default_random_engine re;
-    
     for (int i = 0; i < n; i++) {
-        data[i].x = unifx(re);
-        data[i].y = unify(re);
-        data[i].mass = rand() % 4 + 1;
+        data[i].x = rand() % 100;
+        data[i].y = rand() % 100;
+        data[i].mass = rand() % 3 + 1;
         data[i].color = Scalar(rand() % 255, rand() % 255, rand() % 255);
     }
 }
@@ -53,8 +48,8 @@ void calcForce(Body *p, float dt, int numBodies) {
         double fy = 0.0f;
 
         for (int j = 0; j < numBodies; j++) {
-            double dx = (p[j].x - p[i].x) / 100;
-            double dy = (p[j].y - p[i].y) / 100;
+            double dx = p[j].x - p[i].x;
+            double dy = p[j].y - p[i].y;
             double sqdist = dx*dx + dy*dy + SOFTENING;
             double InvDist = 1.0f / sqrtf(sqdist);
             double InvDist3 = InvDist * InvDist * InvDist;
@@ -69,8 +64,8 @@ void calcForce(Body *p, float dt, int numBodies) {
 }
 
 int main() {
-    double scale_x = 100;
-    double scale_y = 100;
+    double scale_x = WORLD_MAX_X / 100;
+    double scale_y = WORLD_MAX_Y / 100;
 
     Mat disp(Mat(WORLD_MAX_Y, WORLD_MAX_X, CV_8UC3));
     disp = Scalar(128, 128, 128);
@@ -84,17 +79,26 @@ int main() {
     imshow("N-Particles Gravity Simulation", disp);
 
     int step = 0;
+    int prev = 0;
 
     for (int iter = 0; ; iter++) {
         disp = Scalar(0, 0, 0);
         dt = abs(dt);
+        if (prev) {
+            dt *= -1;
+            iter -= 2;
+            if (iter < 0) {
+                iter = 0;
+                goto input;
+            }
+        }
         calcForce(p, dt, nBodies);
 
         #pragma omp parallel for schedule(dynamic)
         for (int i = 0; i < nBodies; i++) {
             p[i].x += (int)p[i].vx * dt;
             p[i].y += (int)p[i].vy * dt;
-            circle( disp, Point(round(p[i].x), round(p[i].y)), p[i].mass, p[i].color, -1);
+            circle( disp, Point(round(p[i].x * scale_x), round(p[i].y * scale_y)), p[i].mass, p[i].color, -1);
         }
 
         putText(disp, "Epoch: " + to_string(iter), Point(10, 30), FONT_HERSHEY_COMPLEX, 0.5, Scalar(255, 255, 255), 1, LINE_8, false);
@@ -106,7 +110,7 @@ int main() {
             return 0;
         } else if (step || c == 'p') {
             step = 0;
-
+            prev = 0;
             while (1) {
                 c = (char)waitKey(0);
                 if (c == 'p')
@@ -120,7 +124,37 @@ int main() {
                     step = 1;
                     break;
                 }
+                else if (c == 'e') {
+                    ofstream outFile;
+                    string filename = "n-body.txt";
+                    outFile.open(filename, ofstream::out | ofstream::trunc);
+                    cout << "e was pressed \n";
+                    if (!outFile.is_open())
+                    {
+                        cout << "n-body.txt not found.";
+                    }
+
+                        for (int i = 0; i < nBodies; i++)
+                        {
+                            outFile << p[i].x << " ";
+                            outFile << p[i].y << " ";
+                            outFile << p[i].mass << " ";
+                            outFile << p[i].vx << " ";
+                            outFile << p[i].vy << " ";
+                            outFile << p[i].color[0] << " ";
+                            outFile << p[i].color[1] << " ";
+                            outFile << p[i].color[2] << " ";
+                            outFile << "\n";
+                        }
+
+                    outFile.close();
+                }/*else if (c == 'b') {
+                    prev = 1;
+                    step = 1;
+                    break;
+                }*/
             }
         }
+
     }
 }
